e721b65dd22a73a713ff62a12dceec42746b9b80
yanfa_liaojx <liaojiaxi@chinatelecom.cn>
2021-02-01 16:51:38 +0800

    feat: 第一次提交

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+<!-- 全局的一个模版，考虑加一些全局的说明 -->
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+/* #app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>
